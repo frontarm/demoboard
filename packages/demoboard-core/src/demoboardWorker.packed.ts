a9523f4637515bbb25309fb7c17ce3a76b5eb6c9
James K Nelson <james@jamesknelson.com>
2019-11-02 20:53:59 +0900

    another try

---
+++ packages/demoboard-core/src/demoboardWorker.packed.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Seven Stripes Kabushiki Kaisha
+ *
+ * This source code is licensed under the Apache License, Version 2.0, found
+ * in the LICENSE file in the root directory of this source tree.
+ */
+
+import * as Comlink from 'comlink'
+import { DemoboardWorker as IDemoboardWorker } from '@frontarm/demoboard-worker'
+import { DemoboardResourceURLs } from './DemoboardResourceURLs'
+
+let worker: IDemoboardWorker
+
+async function getWorker(
+  resourceURLs: DemoboardResourceURLs,
+): Promise<IDemoboardWorker> {
+  if (worker) {
+    return worker
+  }
+
+  let mod =
+    // eslint-disable-next-line import/no-webpack-loader-syntax
+    require('worker-loader!@frontarm/demoboard-worker')
+
+  if (mod.default) {
+    mod = mod.default
+  }
+
+  if (typeof Worker === 'undefined') {
+    // In the test environment, no worker is used, and the underlying
+    // worker object is just returned directly.
+    return mod
+  } else if (mod.isFallback) {
+    return mod(resourceURLs)
+  } else {
+    const DemoboardWorker = new mod()
+    worker = Comlink.wrap(DemoboardWorker)
+    return worker
+  }
+}
+
+export default getWorker
--- packages/demoboard-core/src/demoboardWorker.ts
+++ packages/demoboard-core/src/demoboardWorker.ts
@@ -5,38 +5,5 @@
  * in the LICENSE file in the root directory of this source tree.
  */
 
-import * as Comlink from 'comlink'
-import { DemoboardWorker as IDemoboardWorker } from '@frontarm/demoboard-worker'
-import { DemoboardResourceURLs } from './DemoboardResourceURLs'
-
-let worker: IDemoboardWorker
-
-async function getWorker(
-  resourceURLs: DemoboardResourceURLs,
-): Promise<IDemoboardWorker> {
-  if (worker) {
-    return worker
-  }
-
-  let mod =
-    // eslint-disable-next-line import/no-webpack-loader-syntax
-    require('worker-loader!@frontarm/demoboard-worker')
-
-  if (mod.default) {
-    mod = mod.default
-  }
-
-  if (typeof Worker === 'undefined') {
-    // In the test environment, no worker is used, and the underlying
-    // worker object is just returned directly.
-    return mod
-  } else if (mod.isFallback) {
-    return mod(resourceURLs)
-  } else {
-    const DemoboardWorker = new mod()
-    worker = Comlink.wrap(DemoboardWorker)
-    return worker
-  }
-}
-
+import getWorker from './demoboardWorker.packed'
 export default getWorker
